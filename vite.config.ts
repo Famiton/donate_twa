7b849fb646b959d283ee1cf12b822bd7e7b7b436
Famiton <r3tgw@yandex.ru>
2023-10-11 20:37:57 +0300

    up

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //base:   '/donete_swa/',
+  //base:   '/donete_twa/',
   //build: { outDir: 'docs'},
  
   
