95fdba7d24eefcce6945dd00919d85e71e993dc1
Famiton <r3tgw@yandex.ru>
2023-10-12 11:17:55 +0300

    upd

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.scss'
-
+WebApp.ready()
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
 
-WebApp.ready()
