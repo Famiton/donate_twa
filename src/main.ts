6a0fd4b756308721720997bb8469e7ff66a50ca1
Famiton <r3tgw@yandex.ru>
2023-10-29 14:44:10 +0300

    up

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-//import WebApp from '@twa-dev/sdk'
+import WebApp from '@twa-dev/sdk'
 import { createApp } from 'vue'
 import "primevue/resources/themes/lara-dark-blue/theme.css";
 import './style.css'
 import PrimeVue from 'primevue/config';
 import SendTX from './components/SendTX.vue';
 import App from './App.vue'
-//WebApp.ready()
+WebApp.ready()
+WebApp.expand()
 createApp(App).use(PrimeVue).component('SendTX',SendTX).mount('#app')
