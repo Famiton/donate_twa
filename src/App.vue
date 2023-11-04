e22427823fe9723c9d9d07dd9da49d09d7bf1002
Famiton <r3tgw@yandex.ru>
2023-11-04 14:07:50 +0300

    upd

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import SendTX from './components/SendTX.vue'
 </script>
 
 <template>
- <div class="navbar bg-neutral rounded-2xl" >
+ <div class="navbar bg-neutral rounded-2xl my-1" >
   <div class="navbar-start text-xl text-primary"> Поблагодарить</div>
   <div class="navbar-end"><div id="button-root"></div></div>
 </div>
