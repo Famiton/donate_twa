d7b97588d3518ba6339115f55771634e961df2d2
Famiton <r3tgw@yandex.ru>
2023-11-11 10:20:27 +0300

    upd

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './assets/main.css'
 import WebApp from '@twa-dev/sdk'
 import { createApp } from 'vue'
 import App from './App.vue'
+
+createApp(App).mount('#app')
 WebApp.ready()
 WebApp.expand()
-createApp(App).mount('#app')
